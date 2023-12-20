02550051ed086d64257cf8918f862f2ba98502a4
Taka <fate.of.avel@gmail.com>
2023-12-21 00:01:46 +0900

    プラン表示機能を暫定で作成

--- client/src/MainBody.jsx
+++ client/src/MainBody.jsx
@@ -1,47 +1,55 @@
-function MainBody() {
+function MainBody(props) {
+  const { loading, plan } = props;
+
   return (
-    <>
-      <div className="MainBody">
-        {/* <a href="/">MainBody</a> */}
-        <ul class="timeline">
-          <li>
-            <p class="timeline-date">2019年04月</p>
-            <div class="timeline-content">
-              <br />
-              <p>入社</p>
-            </div>
-          </li>
-          <li>
-            <p class="timeline-date">2019年04月</p>
-            <div class="timeline-content">
-              <br />
-              <p>入社</p>
-            </div>
-          </li>
-          <li>
-            <p class="timeline-date">2019年04月</p>
-            <div class="timeline-content">
-              <br />
-              <p>入社</p>
-            </div>
-          </li>
-          <li>
-            <p class="timeline-date">2019年04月</p>
-            <div class="timeline-content">
-              <br />
-              <p>入社</p>
-            </div>
-          </li>
-          <li>
-            <p class="timeline-date">2019年04月</p>
-            <div class="timeline-content">
-              <br />
-              <p>入社</p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div>
+      {loading ? (
+        <p>ローディング中...</p>
+      ) : plan ? (
+        <div className="MainBody">
+          {/* <a href="/">MainBody</a> */}
+          <ul class="timeline">
+            <li>
+              <p class="timeline-date">2019年04月</p>
+              <div class="timeline-content">
+                <br />
+                <p>入社</p>
+              </div>
+            </li>
+            <li>
+              <p class="timeline-date">2019年04月</p>
+              <div class="timeline-content">
+                <br />
+                <p>入社</p>
+              </div>
+            </li>
+            <li>
+              <p class="timeline-date">2019年04月</p>
+              <div class="timeline-content">
+                <br />
+                <p>入社</p>
+              </div>
+            </li>
+            <li>
+              <p class="timeline-date">2019年04月</p>
+              <div class="timeline-content">
+                <br />
+                <p>入社</p>
+              </div>
+            </li>
+            <li>
+              <p class="timeline-date">2019年04月</p>
+              <div class="timeline-content">
+                <br />
+                <p>入社</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <p>何もない</p>
+      )}
+    </div>
   );
 }
 
