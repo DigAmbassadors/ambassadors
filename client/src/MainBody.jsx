8e63c44150b43c88f13191f789cbdf0ecd0788c6
Taka <fate.of.avel@gmail.com>
2023-12-21 09:49:33 +0900

    写真の追加と文章編集

--- client/src/MainBody.jsx
+++ client/src/MainBody.jsx
@@ -1,3 +1,8 @@
+import portImg from "./assets/image/ishigakiAirport.jpg";
+import kabirawanImg from "./assets/image/kabirawan.jpg";
+import goya from "./assets/image/goyachample.jpg";
+import "./assets/style/App.css";
+
 function MainBody(props) {
   const { loading, plan } = props;
 
@@ -10,38 +15,48 @@ function MainBody(props) {
           {/* <a href="/">MainBody</a> */}
           <ul class="timeline">
             <li>
-              <p class="timeline-date">2019年04月</p>
+              <p class="timeline-date"></p>
               <div class="timeline-content">
                 <br />
-                <p>入社</p>
+                <p>１０：００</p>
+                <p>　新石垣島空港　３０分</p>
+                <img src={portImg} alt="" className="timeline-content-img" />
               </div>
             </li>
             <li>
-              <p class="timeline-date">2019年04月</p>
+              <p class="timeline-date"></p>
               <div class="timeline-content">
                 <br />
-                <p>入社</p>
+                <p>移動　３０分</p>
               </div>
             </li>
             <li>
-              <p class="timeline-date">2019年04月</p>
+              <p class="timeline-date"></p>
               <div class="timeline-content">
                 <br />
-                <p>入社</p>
+                <p>１１：００</p>
+                <p>　川平湾　６０分</p>
+                <img
+                  src={kabirawanImg}
+                  alt=""
+                  className="timeline-content-img"
+                />
               </div>
             </li>
             <li>
-              <p class="timeline-date">2019年04月</p>
+              <p class="timeline-date"></p>
               <div class="timeline-content">
                 <br />
-                <p>入社</p>
+                <p>移動　３０分</p>
               </div>
             </li>
             <li>
-              <p class="timeline-date">2019年04月</p>
+              <p class="timeline-date"></p>
               <div class="timeline-content">
                 <br />
-                <p>入社</p>
+                <p>１２：３０</p>
+                <p>　島唄三味線ライブ居酒屋　結風　６０分</p>
+                <img src={goya} alt="" className="timeline-content-img" />
               </div>
             </li>
           </ul>
