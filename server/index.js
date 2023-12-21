4a6fdd56fc66b32a93817d01c496bb234921095b
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-21 17:46:54 +0900

    aws db接続設定

--- server/index.js
+++ server/index.js
@@ -16,6 +16,17 @@ app.get('/api/test', (req, res) => {
   res.status(200).send('フロント/バックの接続完了');
 });
 
+// データベース接続テストのエンドポイント
+app.get('/api/db-test', async (req, res) => {
+  try {
+    const result = await knex.raw('SELECT 1+1 AS result'); // シンプルなクエリ
+    res.status(200).json({ message: 'データベース接続成功', result: result.rows });
+  } catch (error) {
+    console.error('データベース接続エラー:', error);
+    res.status(500).json({ message: 'データベース接続失敗', error: error.message });
+  }
+});
+
 //テンプレート
 // app.get('/api', async (req, res) => {
 //   await knex('table_name')
